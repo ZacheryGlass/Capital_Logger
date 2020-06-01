63f64a764f79f541fde0898114a5c8d721c23c92
Zachery Glass <ZacheryGlass@pm.me>
2020-06-01 11:33:24 -0500

    Setup basic schema and model skeleton

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
-const app = express();
-const routes = require('./routes/api');
+const mongoose = require('mongoose');
 
-app.use(routes);
+// set up express app.
+const app = express();
+
+mongoose.connect('mongodb://localhost/capital_logger');
+mongoose.Promise = global.Promise;
+
+// parses json request and attach to route handler
+// (order of app.use matters here)
+app.use(express.json());
+
+// initialize api routes
+app.use('/api', require('./routes/api'));
 
 // process is a gloval variable.
 // Use the eviroment variable if it's set, otherwise use port 3000.
