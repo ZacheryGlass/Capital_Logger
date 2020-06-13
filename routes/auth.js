17751e0d8fded0d1d57607ce389c9fa8b85721ca
Zachery Glass <ZacheryGlass@pm.me>
2020-06-12 19:29:17 -0500

    Add GoogleStrategy for passport authentication

--- routes/auth.js
+++ routes/auth.js
@@ -12,11 +12,20 @@ router.get('/logout', (req, res) => {
     res.send('Logging Out.');
 });
 
+// navigating to /auth/google will fire the passport google sign in
+// after the google sign, you will return to the redirect
 router.get(
     '/google',
     passport.authenticate('google', {
         scope: ['profile'],
     })
 );
+
+// when you hit the redirect, you will execture the following 2 functions
+// First is a passort function, which triggers the callback function that is set
+// as part of your GoogleStrategy. Finally, the arrow function below will execute
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+    res.send('callback URI');
+});
 
 module.exports = router;
